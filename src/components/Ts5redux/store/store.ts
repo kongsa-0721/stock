4a0d63ea92cdffc7924a6e668a37e8b992a32694
kongsa-0721 <ks210220364@icloud.com>
2022-01-13 01:04:42 +0800

    dev

--- src/components/Ts5redux/store/store.ts
+++ src/components/Ts5redux/store/store.ts
@@ -1,6 +1,7 @@
 import { reducers } from "./reducer";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+//设置这个才可以使用Chrome-Redux-DevTools
 import { composeWithDevTools } from "redux-devtools-extension";
 
 //第二个参数是state的默认值 可以选择不传 传的话会覆盖掉initialstate
